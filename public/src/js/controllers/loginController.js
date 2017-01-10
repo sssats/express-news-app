80b6f99781a232224beabb4f77c460c112470b41
Ivashkevich, Stanislau <stanislau.ivashkevich@kronos.com>
2017-01-10 13:40:18 +0300

    stringify post  login data

--- public/src/js/controllers/loginController.js
+++ public/src/js/controllers/loginController.js
@@ -7,10 +7,10 @@ class loginController {
     let login = document.querySelector('.login input[name=login]').value;
     let password = document.querySelector('.login input[name=password]').value;
 
-    requestProviderController.post(config.url.api + config.url.login, {
+    requestProviderController.post(config.url.api + config.url.login, JSON.stringify({
       login: login,
       password: password
-    }).then(function(json) {
+    })).then(function(json) {
       console.log(json);
     });
   }
